7cb5bd8068cd104aa383646126d8070320865391
8bit <139103755+RGBXD4@users.noreply.github.com>
2024-07-09 15:19:41 +0800

    uh i think

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1213,8 +1213,8 @@ class PlayState extends MusicBeatState
 		add(botplayTxt);
 	
 	var creditTxt = new FlxText(876, 648, 348);
-     creditTxt.text = "PORTED BY\nFNF BR";
-    creditTxt.setFormat(Paths.font("vcr.ttf"), 30, FlxColor.WHITE, RIGHT, FlxTextBorderStyle.OUTLINE,FlxColor.BLACK);
+    creditTxt.text = "BY RGBXD4";
+    creditTxt.setFormat(Paths.font("vcr.ttf"), 25 FlxColor.BLUE, RIGHT, FlxTextBorderStyle.OUTLINE,FlxColor.BLACK);
     creditTxt.scrollFactor.set();
     add(creditTxt);
 		if(ClientPrefs.downScroll) {
