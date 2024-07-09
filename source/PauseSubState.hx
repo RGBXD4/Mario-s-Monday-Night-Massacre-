658c0acc82d81a9c87cbfb6d53c5c2b05767dfcc
8bit <139103755+RGBXD4@users.noreply.github.com>
2024-07-09 08:11:02 +0800

    why it cant work later first song w

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -268,17 +268,13 @@ class PauseSubState extends MusicBeatSubstate
 		var accepted = controls.ACCEPT;
 
 		if (upP) {
-		if changeSelection(-1)
-			{	
-		         FlxG.sound.play(Paths.sound('scrollMenu' + prefix), 1);
-		        }
+	        changeSelection(-1);
+		FlxG.sound.play(Paths.sound('scrollMenu' + prefix), 1);
 		}
 		
-		if (downP) { 
-		if changeSelection(1)
-			{
-		         FlxG.sound.play(Paths.sound('scrollMenu' + prefix), 1);
-			}
+		if (downP) {
+		changeSelection(1);
+	        FlxG.sound.play(Paths.sound('scrollMenu' + prefix), 1);
 		}
 
 		selectLeft.x = pageSprite.x + 70 - selectLeft.width + (Math.sin(selectTimer * 7) * 3);
