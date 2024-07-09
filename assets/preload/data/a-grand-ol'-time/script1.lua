f987c47c94cff464997f1a9042f6f2c02d875138
8bit <139103755+RGBXD4@users.noreply.github.com>
2024-07-09 15:02:17 +0800

    Delete assets/preload/data/a-grand-ol'-time directory

--- assets/preload/data/a-grand-ol'-time/script1.lua
+++
@@ -1,4 +0,0 @@
-function onCreate()
-	setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'Grand_dad_game_over');
-	setPropertyFromClass('GameOverSubstate', 'endSoundName', 'grand_dad_game_over_end');
-end
