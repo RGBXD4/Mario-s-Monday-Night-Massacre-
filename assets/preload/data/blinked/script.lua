d06c6f8944c0093322b74f5a400bbcd85f0162d4
8bit <139103755+RGBXD4@users.noreply.github.com>
2024-07-09 17:57:54 +0800

    Update script.lua

--- assets/preload/data/blinked/script.lua
+++ assets/preload/data/blinked/script.lua
@@ -2,30 +2,30 @@ local fx = 0
 function onCreatePost()
 	local location = 'misc/daiseye/'
     for i=1,3 do
-        makeLuaSprite('eye'..i, location..'Eye'..i, 0, 0)
+        makeLuaSprite('eye'..i, location..'eye'..i, 0, 0)
         scaleObject('eye'..i, 1.4, 1.4)
         setObjectCamera('eye'..i, 'hud')
         screenCenter('eye'..i, 'xy')
         addLuaSprite('eye'..i, true)
 
-        makeLuaSprite('Pupil'..i, location..'Pupil', 0, 0)
-        scaleObject('Pupil'..i, 1.4, 1.4)
-        setObjectCamera('Pupil'..i, 'hud')
-        screenCenter('Pupil'..i, 'xy')
-        addLuaSprite('Pupil'..i, true)
+        makeLuaSprite('pupil'..i, location..'pupil', 0, 0)
+        scaleObject('pupil'..i, 1.4, 1.4)
+        setObjectCamera('pupil'..i, 'hud')
+        screenCenter('pupil'..i, 'xy')
+        addLuaSprite('pupil'..i, true)
     end
     
-    setProperty('Pupil3.x', 300)
-    setProperty('Pupil3.y', -275)
+    setProperty('pupil3.x', 300)
+    setProperty('pupil3.y', -275)
 
-    setProperty('Pupil2.x', 600)
-    setProperty('Pupil2.y', -100)
-    setProperty('Pupil1.alpha', 0)
-    setProperty('Pupil2.alpha', 0)
-    setProperty('Pupil3.alpha', 0)
-    setProperty('Eye1.alpha', 0)
-    setProperty('Eye2.alpha', 0)
-    setProperty('Eye3.alpha', 0)
+    setProperty('pupil2.x', 600)
+    setProperty('pupil2.y', -100)
+    setProperty('pupil1.alpha', 0)
+    setProperty('pupil2.alpha', 0)
+    setProperty('pupil3.alpha', 0)
+    setProperty('eye1.alpha', 0)
+    setProperty('eye2.alpha', 0)
+    setProperty('eye3.alpha', 0)
 end
 
 function onUpdate(elapsed)
